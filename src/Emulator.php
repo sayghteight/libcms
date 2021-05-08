7408d11614320ab56534537108d975c474976fbe
Darthar <duelista16@gmail.com>
2021-05-08 03:51:27 +0200

    prueba

--- src/Emulator.php
+++ src/Emulator.php
@@ -4,5 +4,23 @@ namespace wowcms\libcms;
 
 class Emulator
 {
+    protected $tables = 
+    [
+        // AUTH DATABASE
+        "account" => "account",
+        "account_access" => "account_access",
+        "account_banned" => "account_banned",
+        "battlenet_accounts" => "battlenet_accounts",
+        "realmlist" => "realmlist",
+
+    ];
+
+
+    public function getTableArray($name)
+    {
+        if (array_key_exists($name, $this->tables)): 
+            return $this->tables[$name];
+        endif;
+    }
 }
 
